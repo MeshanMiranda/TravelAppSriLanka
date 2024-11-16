4ecd820c90a42d5d18958b8404bf807d846ee17a
Meshan Miranda <168061423+MeshanMiranda@users.noreply.github.com>
2024-11-16 18:46:39 +0530

    NewFile

--- app/src/main/java/com/example/travel/Activity/BaseActivity.java
+++ app/src/main/java/com/example/travel/Activity/BaseActivity.java
@@ -1,23 +1,21 @@
-package com.example.travel;
+package com.example.travel.Activity;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class BaseActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
-        Window w=getWindow();
+        database = FirebaseDatabase.getInstance();
+        Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
