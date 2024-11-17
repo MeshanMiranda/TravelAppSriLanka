8d7602c6fc608139d406511ed6112121eb3654c8
Meshan Miranda <168061423+MeshanMiranda@users.noreply.github.com>
2024-11-17 10:04:19 +0530

    Padding 8dp

--- app/src/main/java/com/example/travel/Activity/BaseActivity.java
+++ app/src/main/java/com/example/travel/Activity/BaseActivity.java
@@ -19,4 +19,6 @@ public class BaseActivity extends AppCompatActivity {
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
+    //https://firebasestorage.googleapis.com/v0/b/project189-ede2a.appspot.com/o/banner1.png?alt=media&token=f525f102-1a82-4bc3-b697-b9d3b2e85e26
+    //https://firebasestorage.googleapis.com/v0/b/project189-ede2a.appspot.com/o/banner2.png?alt=media&token=898ca000-1f21-42df-84e1-e073ec464ae7
 }
