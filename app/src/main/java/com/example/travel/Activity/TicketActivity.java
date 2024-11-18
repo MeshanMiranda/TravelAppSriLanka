77f92d353119bdae35c9036d8fc7831aae7977bd
Meshan Miranda <168061423+MeshanMiranda@users.noreply.github.com>
2024-11-18 13:41:48 +0530

    Complete Full App

---
+++ app/src/main/java/com/example/travel/Activity/TicketActivity.java
@@ -0,0 +1,73 @@
+package com.example.travel.Activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.example.travel.Domain.ItemDomain;
+import com.example.travel.R;
+import com.example.travel.databinding.ActivityTicketBinding;
+
+public class TicketActivity extends BaseActivity {
+    ActivityTicketBinding binding;
+    private ItemDomain object;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTicketBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getIntentExtra();
+        setVariable();
+
+    }
+
+    private void setVariable() {
+        Glide.with(TicketActivity.this)
+                .load(object.getPic())
+                .into(binding.pic);
+
+        Glide.with(TicketActivity.this)
+                .load(object.getTourGuidePic())
+                .into(binding.profile);
+
+        binding.titleTxt.setText(object.getTitle());
+        binding.backBtn.setOnClickListener(v -> finish());
+        binding.durationTxt.setText(object.getDuration());
+        binding.tourGuideTxt.setText(object.getDateTour());
+        binding.timeTxt.setText(object.getTimeTour());
+        binding.tourGuideNameTxt.setText(object.getTourGuideName());
+
+        binding.messageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:" + object.getTourGuidePhone()));
+                sendIntent.putExtra("sms_body", "type your message");
+                startActivity(sendIntent);
+            }
+        });
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = object.getTourGuidePhone();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void getIntentExtra() {
+        object = (ItemDomain) getIntent().getSerializableExtra("object");
+    }
+}
